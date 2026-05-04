e0f3fdcef5a5a4648e795d2e50fa996aed4d6693
Viraj Jayasiri <viraj.jayasiri.work@gmail.com>
2026-05-04 23:34:18 +0530

    Initialize Spring Boot project with Maven wrapper and basic configuration files

---
+++ src/main/java/com/connecthub_springboot_react/ConnecthubSpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package com.connecthub_springboot_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnecthubSpringbootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnecthubSpringbootReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connecthub_springboot_react/ConnecthubSpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connecthub_springboot_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecthubSpringbootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
