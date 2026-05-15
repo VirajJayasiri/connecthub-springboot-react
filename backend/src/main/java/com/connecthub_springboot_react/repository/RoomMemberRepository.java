ddfe5e6f12ce04ca71aa866cedebd8dfc4271e3f
Viraj Jayasiri <viraj.jayasiri.work@gmail.com>
2026-05-15 20:51:58 +0530

    change findByRoomIdAndUserId method to return a list of RoomMembers instead of an Optional

--- backend/src/main/java/com/connecthub_springboot_react/repository/RoomMemberRepository.java
+++ backend/src/main/java/com/connecthub_springboot_react/repository/RoomMemberRepository.java
@@ -4,13 +4,12 @@ import com.connecthub_springboot_react.model.room.RoomMember;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RoomMemberRepository extends MongoRepository<RoomMember, String> {
 
     List<RoomMember> findByRoomId(String roomId);
 
-    Optional<RoomMember> findByRoomIdAndUserId(String roomId, String userId);
+    List<RoomMember> findByRoomIdAndUserId(String roomId, String userId);
 
     long countByRoomId(String roomId);
 
